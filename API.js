1fc3f65fc7d92be1b87a3954628a2b357a94e748
JD18082000 <pdo8@uic.edu>
2022-03-30 09:01:20 -0500

    commit

--- API.js
+++ API.js
@@ -1,13 +1,14 @@
-function getValue() {
-    $.ajax({
-        type:"Get",
-        dataType: 'json',
-        url: "https://genius.p.rapidapi.com/artists/16775/songs",
-        async: false,
-        crossDomain:true,
-        
-        success: function(response){
-            console.log(response);
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://genius.p.rapidapi.com/artists/16775/songs",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Host": "genius.p.rapidapi.com",
+            "X-RapidAPI-Key": "3a7a7308cfmsheec7ee161b8ed7ep129edejsn4facaaf88f50"
         }
-    });
-}
+
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    })
